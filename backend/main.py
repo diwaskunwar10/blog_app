26e1e3f251a6e48b2c3f8653cde3a7a58d019779
Diwas Kunwar <diwas.kuwar@gmail.com>
2024-04-29 16:31:21 +0545

    updating main.py

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,6 @@ app.include_router(auth.router)
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
 
